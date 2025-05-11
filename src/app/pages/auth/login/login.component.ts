36a3d8fb166f7af20855896629659f4bf0c9407d
diyaa haik <diyaahaik77@gmail.com>
2025-05-11 16:03:16 +0300

    login and api

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../shared/services/auth.service';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -13,11 +14,12 @@ export class LoginComponent {
 loginForm:FormGroup;
 submitted = false;
 errorMessage = '';
+ScssMessage= '';
 fadeOut = false;
 
 
 
-constructor(private fb: FormBuilder, private router: Router) {
+constructor(private fb: FormBuilder, private router: Router ,private authService: AuthService   ) {
   this.loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required ]
@@ -27,29 +29,71 @@ constructor(private fb: FormBuilder, private router: Router) {
 
 
 
+// onSubmit() {
+//   this.submitted = true;
+//     if (this.loginForm.invalid) return;
+
+//     const { email, password } = this.loginForm.value;
+
+//     this.authService.login(email!, password!).subscribe(user => {
+//       if (user) {
+//         localStorage.setItem('userRole', user.role);
+//         this.router.navigate(['/dashboard']);
+//       }
+
+//     else {
+//       this.errorMessage = 'البريد الإلكتروني أو كلمة المرور غير صحيحة';
+// this.fadeOut = false;
+
+// setTimeout(() => {
+//   this.fadeOut = true;
+// }, 4500); // نبدأ الاختفاء قبل أن نخفي النص
+
+// setTimeout(() => {
+//   this.errorMessage = '';
+//   this.fadeOut = false;
+// }, 5000);
+//     }
+
+// }
+// }
+
+
 onSubmit() {
   this.submitted = true;
+  if (this.loginForm.invalid) return;
 
-    if (this.loginForm.invalid) return;
+  const { email, password } = this.loginForm.value;
 
-    const { email, password } = this.loginForm.value;
-
-    if (email === 'admin@example.com' && password === '123456') {
-      localStorage.setItem('userRole', 'admin');
+  this.authService.login(email!, password!).subscribe(user => {
+    if (user) {
+      localStorage.setItem('userRole', user.role);
       this.router.navigate(['/dashboard']);
-    } else {
-      this.errorMessage = 'البريد الإلكتروني أو كلمة المرور غير صحيحة';
-this.fadeOut = false;
+this.ScssMessage = 'تم تسجيل الدخول بنجاح';
 
 setTimeout(() => {
   this.fadeOut = true;
-}, 4500); // نبدأ الاختفاء قبل أن نخفي النص
+}, 4500);
 
 setTimeout(() => {
-  this.errorMessage = '';
-  this.fadeOut = false;
+  this.ScssMessage = '';
+ this.fadeOut = false;
 }, 5000);
-    }
 
+
+    }    else {
+       this.errorMessage = 'البريد الإلكتروني أو كلمة المرور غير صحيحة';
+ this.fadeOut = false;
+
+ setTimeout(() => {
+   this.fadeOut = true;
+ }, 4500); // نبدأ الاختفاء قبل أن نخفي النص
+
+ setTimeout(() => {
+   this.errorMessage = '';
+  this.fadeOut = false;
+ }, 5000);
+     }
+  });
 }
 }
